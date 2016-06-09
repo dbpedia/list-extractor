96270c8ad08e14f9ab114806ea5e142c7385ad99
fed_bai <finalfed@hotmail.it>
2016-06-09 16:17:25 +0200

    Refactoring and bug fix. - created new a main module "listExtractor" - created accessory module "utilities" - wikiParser now handles nested lists and empty sections - producing a .txt file with parsed content - tested  successfully on en:List_of_works_of_William_Gibson, en:William_Gibson it:William_Gibson

--- wikiParser.py
+++ wikiParser.py
@@ -13,58 +13,60 @@ def jsonpedia_req(req):
     parsed_ans = json.loads(answer)
     return parsed_ans
 
+
 '''
 returns a dictionary with section names as keys and their list contents as values
 '''
-def parse_section(section, title) :
-    section_lists = {} #initializing dictionary
 
-    #parse only if there is available content
-    if ('content' in section and section['content'] != "") :
+
+def parse_section(section, title):
+    section_lists = {}  # initializing dictionary
+
+    # parse only if there is available content
+    if ('content' in section and section['content'] != ""):
         """
         #for sections with empty content, acting only as header
         if section['content'] == "" and 'title' in section :
             title = section['title']
             section_lists[title] = ""
         """
-        #checks whether to concatenate the title or not
-        if section['level'] > 0 : #represents a nested section
-            title = str(title) + " - " + section['title'] #concatenate titles
+        # checks whether to concatenate the title or not
+        if section['level'] > 0:  # represents a nested section
+            title = str(title) + " - " + section['title']  # concatenate titles
 
-        else :
+        else:
             title = section['title']
             section_lists[title] = ""
 
-
-        #don't consider keys since they are non-relevant (e.g. an0, an1,..)
+        # don't consider keys since they are non-relevant (e.g. an0, an1,..)
         content = section['content'].values()
 
-        sect_list = [] #will contain the list extracted from current section
+        sect_list = []  # will contain the list extracted from current section
 
         '''Extract section content - values inside dictionary inside 'content' key '''
-        for val in content :
-            if ('@type' in val) :
-                if (val['@type'] == 'list') :
-                    level = 1 #level is used to keep trace of list inception
-                    nest_list = [] #will contain a nested list if there is one
+        for val in content:
+            if ('@type' in val):
+                if (val['@type'] == 'list'):
+                    level = 1  # level is used to keep trace of list inception
+                    nest_list = []  # will contain a nested list if there is one
 
-                    #pass list elements to be parsed
-                    for cont in val['content'] :
-                        #check if current list element is nested
-                        if ('level' in cont and cont['level'] > level) :
+                    # pass list elements to be parsed
+                    for cont in val['content']:
+                        # check if current list element is nested
+                        if ('level' in cont and cont['level'] > level):
                             level = cont['level']
-                            #call parse_list on nested list and store it in nest_list variable
+                            # call parse_list on nested list and store it in nest_list variable
                             nest_cont = parse_list(cont)
                             nest_list.append(nest_cont)
-                        else :
-                            #if there is nested content, append it like a nested list
-                            if nest_list != [] :
+                        else:
+                            # if there is nested content, append it like a nested list
+                            if nest_list != []:
                                 sect_list.append(nest_list)
                                 nest_list = []
                                 level = 1
                             sect_list.append(parse_list(cont))
 
-                    #adds a new voice in the dictionary representing list in the given section
+                    # adds a new voice in the dictionary representing list in the given section
                     section_lists[title] = sect_list
                 """
                 elif (title not in section_lists) :  ##maybe useless?
@@ -76,29 +78,31 @@ def parse_section(section, title) :
                 section_lists[title] = ""
             """
 
-
     return section_lists
 
 
 '''
 returns a parsed list element
 '''
-def parse_list(list_elem) :
+
+
+def parse_list(list_elem):
     """
     Parses a list element extracting relevant info
     :param list_elem: current list item in json format
     :return: a string containing useful info from list element
     """
 
-    list_content = "" #initializing output
+    list_content = ""  # initializing output
 
     if ('content' in list_elem and list_elem['content'] != ""):
 
-        for cont in list_elem['content'] :
-            if ('label' in cont): # if there is a label key, take only its value
+        for cont in list_elem['content']:
+            if ('label' in cont):  # if there is a label key, take only its value
                 cont = cont['label']
                 list_content += cont
-            elif ('@type' in cont and cont['@type'] != 'list_item'): # leave templates and links as they are, as it's structured info
+            elif ('@type' in cont and cont[
+                '@type'] != 'list_item'):  # leave templates and links as they are, as it's structured info
                 cont_type = cont['@type']
                 if (cont_type == 'link') or (cont_type == 'template'):
                     list_content += str(cont)
@@ -108,11 +112,12 @@ def parse_list(list_elem) :
                 curr_list.append(parse_list(cont['content']))
                 list_content= str(cont)
                 '''
-            elif ('attributes' not in cont) :  # Take everything else but ignore bottom page references
+            elif ('attributes' not in cont):  # Take everything else but ignore bottom page references
                 list_content += cont
     return list_content
 
-def mainParser(language, resource) :
+
+def mainParser(language, resource):
     """
     Main method, returns final dictionary containing all extracted data from given resource in given language
     :param language: language of the resource we want to parse (e.g. it, en, fr...)
@@ -121,7 +126,7 @@ def mainParser(language, resource) :
     """
 
     input = language + ":" + resource
-    #JSONpedia call to retrieve sections  - in this way I can retrieve both section titles and their lists
+    # JSONpedia call to retrieve sections  - in this way I can retrieve both section titles and their lists
     jsonpediaURL_sect = "http://jsonpedia.org/annotate/resource/json/" + input + "?filter=@type:section&procs=Structure"
     try:
         sections = jsonpedia_req(jsonpediaURL_sect)
@@ -149,11 +154,11 @@ def mainParser(language, resource) :
                         # if it's a nested section, concatenate its name with last header title
                         parsed_sect = parse_section(res, header_title)
                         lists.update(parsed_sect)
-            cleanlists = utilities.clean_dictionary(lists) #clean resulting dictionary and leave only meaningful keys
+            cleanlists = utilities.clean_dictionary(lists)  # clean resulting dictionary and leave only meaningful keys
             return cleanlists
 
 
 
 
 
-#print(mainParser('en', 'List_of_works_of_William_Gibson'))
+            # print(mainParser('en', 'List_of_works_of_William_Gibson'))
